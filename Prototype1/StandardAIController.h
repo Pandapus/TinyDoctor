bb588fa980ef4a430b20343151c521f5b57b1c79
Nils Petter Skalerud <np_skalerud@hotmail.com>
2017-02-21 12:12:54 +0100

    Lastet opp tidlig versjon av bevegelse, skyting og AI

---
+++ Prototype1/StandardAIController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "StandardAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYDOCTORTEST_API AStandardAIController : public APlayerController
+{
+	GENERATED_BODY()
+
+};
